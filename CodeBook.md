e0996e46caeb2f247543b1246c561363b2eb7298
eldube <eldube@uniswa.sz>
2014-06-21 18:56:36 +0200

    project second submission

---
+++ CodeBook.md
@@ -0,0 +1,164 @@
+# CodeBook for a Consolidated Human Activity Recognition Using Smartphones Dataset Version 1.0 Dataset
+
+## Purpose
+
+The purpose of the project was to consolidate the Human Activity Recognition Using Smartphones Dataset Version 1.0, which is stored in several
+files and folders, into a single tidy dataset. Described below is the original data and the steps taken to consolidate the dataset into a single dataset. For each variable in the consolidated dataset, a brief description in given.
+
+## The Original Data
+
+The original data is the Human Activity Recognition Using Smartphones Dataset Version 1.0 available from 
+www.smartlab.ws and described by Anguita D. et. al in the following official reference publication:
+
+*Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz.*
+**Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine.**
+International Workshop of Ambient Assisted Living (IWAAL 2012). 
+Vitoria-Gasteiz, Spain. Dec 2012 
+
+According to the documentation, experiments were carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (**WALKING, WALKING-UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING**) wearing a smartphone (Samsung Galaxy S II) on the waist. Details of how the data was collected and recorded can be obtained from the documentations at this URL : http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+## Downloading the Original Dataset
+
+A compressed data file can be downloaded from the following URL : https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip. When this file is unzipped several files are extracted into a folder called **UCI HAR Dataset**.  This folder contains two subfolders which randomly divides the dataset into training data (70% of the dataset stored in subfolder called **train** ) and test data (30% of the dataset stored in subfolder called **test**).Furthers details on the contents of the three folders  can be obtained from the documentation (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones). In the sections that follow, we will simple refer to these three folders, and the specific files used in this project.
+
+### Contents of folder UCI HAR Dataset
+This folder contains information common to both the training data and test data in the data set.
+
+- **README.txt** : a overall description of the experiment
+- **features_info.txt**: Shows information about the variables used on the feature vector.
+- **features.txt**: List of all features.
+- **activity_labels.txt**: Links the class labels with their activity name.
+
+### Contents of subfolder train 
+This folder contains information on the training data  which constitute 70% of the dataset.
+- **train/X_train.txt**: Training set data 
+- **train/y_train.txt**: Training labels.
+- **train/subject_train.txt**: Each row identifies the subject/volunteer who performed the activity. Its range is from 1 to 30. 
+
+
+### contents of subfolder train 
+his folder contains information on the test data  which constitute 30% of the dataset.
+- **test/X_test.txt**: Test set.
+- **test/y_test.txt**: Test labels.
+- **test/subject_train.txt**: Each row identifies the subject/volunteer who performed the activity. Its range is from 1 to 30.
+
+The purpose of this project was to consolidate the information contained in these files and folders into a single dataset that could be 
+easier to use for further analysis. The section that follows outlines the steps taken to generate the consolidated dataset.
+
+
+## Transformation and Consolidating the Data
+
+
+Firstly, a  compressed file of the original dataset was downloaded from the provided URL : "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" . , and uncompressed into a local working directory. The R code used to download the file, and perform
+the  other steps explained below, can be viewed from the R script file : **run_analysis.R**) 
+
+Secondly, the general data available in the folder called **UCI HAR Dataset** was extracted and processed using R as followed.  
+
+1. Write R commands to read the feature variable names from **features.txt** file from the UCI HAR Dataset folder.These names are the same for both training and test data
+
+2. Write R commands to reading activity label names from **activity_labels**.txt file. These labels are the same for both training and test data
+
+
+Thirdly, each set of data( training and test) were, initially, processed separately and merged later as follows:
+
+### Reading and processing the training data
+
+The training data is stored in three text files (**X_train.txt, y_train.txt, subject_train.txt**) which are all found in the **./UCI HAR Dataset/train** folder. The following steps were performed.
+
+3. Write R commands to read the coded training data labels from the file **y_train.txt** 
+4. Write R commands to transform the coded labels to matching descriptive activity names as tabulated in the the file **activity_labels.txt** and read in previous step above.
+5. Write R commands to read the training data subject/volunteer details from the file **subject_train.txt** 
+
+6. Write R commands to read the test data from the file **X_train.txt**
+7. Write R commands to add descriptive column names to the training data using information from the **features.txt** file that was read in sa previous step above.
+8. Write R commands to extract only the measurements on mean and standard deviation for each measurent in the training data set 
+
+### Reading and processing the test data
+
+Similarly, the test data is stored in three text files (**X_test.txt, y_test.txt, subject_test.txt**) which all are all stored in the **./UCI HAR Dataset/test** folder. The following steps were performed.
+
+8. Write R commands to read the coded test data labels from the file **y_test.txt**
+9. Write R commands to transform coded labels to matching descriptive activity names as tabulated in the the file 
+ **activity_labels.txt** and read in a previous step above.
+10. Write R commands to read test data subject/volunteer details from **subject_test.txt** file
+
+11. Write R commands to read the test data from the **X_test.txt** file
+12. Write R commands to add descriptive column names to the test data using information from the file**features.txt** that was read in
+a previous step above.
+13. Write R commands to extract only the measurements on mean and standard deviation for each measurent in the test data set
+
+14. Merge the training and test data obtained togethert to from one dataset.
+15. Make the column names more readable by changing renaming and removing unnecessary symbols.
+16. Save the merged dataset to a file, called **WearableData.RData** , that can be loaded into an R session later using the **load()**
+command.
+17. Create a separate dataset that contains only the average of each variable in the **WearableData.RData** dataset for each activity and each 
+subject/volunteer. Save this dataset to a file called **WearableData.RData**.
+
+
+## Variables of **WearableData** dataset
+
+The **WearableData.RData**.RData** contains the tidy dataset obtained after step 16 above and can be loaded into an R session using the load command as shown below. The variables of this dataset are also shown using the **names()** function in R. 
+
+
+```r
+load("WearableData.RData")
+```
+
+```
+## Warning: cannot open compressed file 'WearableData.RData', probable reason
+## 'No such file or directory'
+```
+
+```
+## Error: cannot open the connection
+```
+
+```r
+names(WearableData)
+```
+
+```
+## Error: object 'WearableData' not found
+```
+
+
+### Details of each variable:
+
+1. **subjectid** : a unique integer value [1-30] used as an identifier for each of the 30 subjects/volunteers in  the experiment.
+2. **description** : a unique description of the activity performed by each subject/volunteer. The value could be 
+(**WALKING, WALKING-UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING**)
+3. **other variables** : detailed description of each of the other variables in this dataset can be obtained from this URL : http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+
+## Variables of **WearableDataAvg.RData** dataset
+The **WearableDataAvg.RData** contains the tidy dataset obtained after step 16 above and can be loaded into an R session using the load command as shown below. The variables of this dataset are also shown using the **names()** function in R. 
+
+
+```r
+load("WearableDataAvg.RData")
+```
+
+```
+## Warning: cannot open compressed file 'WearableDataAvg.RData', probable
+## reason 'No such file or directory'
+```
+
+```
+## Error: cannot open the connection
+```
+
+```r
+names(WearableDataAvg)
+```
+
+```
+## Error: object 'WearableDataAvg' not found
+```
+
+
+### Details of each variable:
+
+1. **subjectid** : a unique integer value [1-30] used as an identifier for each of the 30 subjects/volunteers in  the experiment.
+2. **description** : a unique description of the activity performed by each subject/volunteer. The value could be 
+(**WALKING, WALKING-UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING**)
+3. **other variables** : each other variable in this dataset records the average of a corresponding variable in the **WearableData.RData** dataset discussed in the previous section.
---
+++ run_analysis.R
@@ -0,0 +1,203 @@
+##################################################################################################
+## run_analysis.R                                                                               ##
+## GETTING AND CLEANING DATA PROJECT                                                            ##
+##                                                                                              ##
+##################################################################################################
+
+##################################################################################################
+## DOWNLOADING THE FILES                                                                        ##
+##################################################################################################
+# Download  zipped file 
+wearableDataZipfile <- "projData.zip"
+if(!file.exists(wearableDataZipfile) )  {
+  fileUrl <- 
+    "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileUrl, destfile = wearableDataZipfile, method = "internal")
+}
+
+
+## stop if downloading failed or  file "projData.zip" not in working directory
+if( !file.exists(wearableDataZipfile) ) {
+  stop("Error: StormDataZipfile file is not in the working directory.")
+}else {
+  ##unzip zip source; files extracted to folder "./UCI HAR Dataset"
+  unzip(wearableDataZipfile)
+}
+## After extraction, the dataset file are stored in a folder called UCI HAR Dataset,
+## which further contains two subfolders which randomly divides the dataset into training 
+## data (70% of the dataset stored in subfolder "train") and test data (30% of the dataset 
+## stored in subfolder "test".
+
+##Since the data is stored in subfolders, the following variables were used for easy 
+##reference to path of the data set files.
+
+
+UCIdataPath <- "./UCI HAR Dataset"  ## main folder;contains folders "train"and "test"
+trainDataPath <-  "./UCI HAR Dataset/train" ## path to training data; 70% of data
+testDataPath <-  "./UCI HAR Dataset/test" ## path to training data; 30% of data
+
+## Reading feature variable names from features.txt file
+## first read feature variable names; from features.txt file
+## feature variable names are the same for both training and test data
+
+feature <- read.table(paste(UCIdataPath,"/features.txt", sep=""), 
+              colClasses = c("integer","character"), col.names= c("seqNum", "name"), header = FALSE)
+
+
+## Reading activity labels names from activity_labels.txt file
+## activity labels are the same for both training and test data
+## The activity labels have a code (integer between 1 and 6) and a name
+
+activityLabel <- read.table(paste(UCIdataPath,"/activity_labels.txt", sep=""), 
+               colClasses = c("integer", "character"), col.names= c("code", "name"), header = FALSE)
+
+######################################################################################################
+## Reading the traing data from "./UCI HAR Dataset/train" folder                                    ##
+## The dataset data is stored in two text files                                                     ##
+##  1. X_train.txt : Test Set                                                                       ##
+##  2. y_train.txt : Training labels                                                                ##
+##  3. subject_train.txt : Volonteer details                                                        ##
+######################################################################################################
+
+
+
+######################################################################################################
+## TASK 3 : ADD DESCRIPTIVE ACTIVIY NAMES TO TRAINING DATA                                          ##
+######################################################################################################
+##  first read coded training data labels and tranform codes to matching descriptive 
+## activity names as tabluated in the the activity labels file (activity_labels.txt)
+trainingLabel <- read.table(paste(trainDataPath,"/y_train.txt", sep=""), 
+                                                    col.names= c("code"), header = FALSE)
+
+trainingLabel$description <- apply(trainingLabel, 1, function (i) {
+     targetActivity <- activityLabel[(activityLabel$code==i),]
+     return(targetActivity$name)}  )
+
+## read training data subject/volunteer details from subject_train.txt file
+trainingSubjectData <- read.table(paste(trainDataPath,"/subject_train.txt", sep=""), 
+                                  colClasses = c("integer"), col.names= c("subjectid"), header = FALSE)
+
+
+## read trainig set data
+trainingData <- read.table(paste(trainDataPath,"/X_train.txt", sep=""))
+
+######################################################################################################
+## TASK 4 : ADD DESCRIPTIVE COLUMN/VARIABLE NAMES TO TRAINING DATA  SET                             ##
+######################################################################################################
+## add descriptive column names to training data using information from features.txt
+names(trainingData) <- make.names(feature$name)
+
+## add label descrptions as first column of trainingData; new data should have 562 columns
+description <- trainingLabel$description
+trainingData <- cbind(description,trainingData)
+
+## add subjectid column as first column of testData using information extracted from subject_train.txt
+subjectid <- trainingSubjectData$subjectid
+trainingData <- cbind(subjectid,trainingData)
+
+######################################################################################################
+## Reading the test data from "./UCI HAR Dataset/test" folder                                       ##
+## The dataset data is stored in two text files                                                     ##
+##  1. X_test.txt : Test Set                                                                        ##
+##  2. y_test.txt : Training labels                                                                 ##
+##  subject_train.txt : Volonteer details                                                           ##
+######################################################################################################
+
+
+
+################################################################## ###################################
+## TASK 3 : ADD DESCRIPTIVE ACTIVIY NAMES TO TEST DATA                                              ##
+######################################################################################################
+##  first read coded test data labels and tranform codes to matching descriptive 
+## activity names as tabluated in the the activity labels file (activity_labels.txt)
+testLabel <- read.table(paste(testDataPath,"/y_test.txt", sep=""),col.names= c("code"),header = FALSE)
+
+
+testLabel$description <- apply(testLabel, 1, function (i) {
+                              targetActivity <- activityLabel[(activityLabel$code==i),]
+                              return(targetActivity$name)}  )
+
+## read training data subject/volunteer details from subject_test.txt file
+testSubjectData <- read.table(paste(testDataPath,"/subject_test.txt", sep=""),
+                              colClasses = c("integer"), col.names= c("subjectid"), header = FALSE)
+
+## read trainig set data
+testData <- read.table(paste(testDataPath,"/X_test.txt", sep=""))
+
+
+
+######################################################################################################
+## TASK 4 : ADD DESCRIPTIVE COLUMN/VARIABLE NAMES TO TEST DATA   SET                               ##
+######################################################################################################
+
+## add descriptive column names to test data using information from features.txt
+names(testData) <- make.names(feature$name)
+
+## add label descrptions as first column of testData ; should now have 562 columns
+description <- testLabel$description
+testData <- cbind(description,testData)
+
+## add subjectid column as first column of testData
+subjectid <- testSubjectData$subjectid
+testData <- cbind(subjectid,testData)
+
+######################################################################################################
+## TASK 2 :EXTRACT ONLY MEASUREMENTS ON MEAN AND STANDARD DEVIATION FOR EACH MEASUREMENT            ##
+######################################################################################################
+
+## find columns measuring mean and std
+columnNames <- names(trainingData)
+stdColumns <- grep("std", columnNames)
+meanColumns <- grep("mean", columnNames)
+targetColumns <- sort(c(1,2,stdColumns, meanColumns)) ##include column 1&2--for subjectid & description 
+
+## subset target columns from trainingData data frame; new data frame should have 81 columns
+mean_std_trainingData <- trainingData[,targetColumns]
+
+## subset target columns from testData data frame; new data frame should have 80 columns
+mean_std_testData <- testData[,targetColumns]
+
+## remove some R objects that are no longer needed to free memory space
+#rm( activityLabel,columnNames, description,feature,meanColumns,  stdColumns, targetColumns)
+#rm(testLabel, trainingLabel, wearableDataZipfile)
+
+######################################################################################################
+## TASK 1: MERGE/COMBINE TRAINING DATA AND TEST DATA                                                ##
+######################################################################################################
+tidyData <- rbind (mean_std_trainingData, mean_std_testData ) ## should have 10299 rows, 81 columns
+
+
+######################################################################################################
+## TASK 4: MAKE COLUMN/VARIABLE NAMES MORE READABLE                                                 ##
+######################################################################################################
+columnNames <- names(tidyData)
+sub1 <- sub ("\\.\\.\\.", "\\.", columnNames) ## replace 3 consecutive periods with a single period
+sub2 <- sub ("\\.\\.", "\\.", sub1)           ## replace 3 consecutive periods with a single period
+sub3 <- sub ("\\.$", "", sub2)         ## replace period that appears at the end of a name
+columnNames <- sub3
+names(tidyData) <- columnNames
+
+## save tidyData to a file
+WearableData <- tidyData
+save(WearableData, file="WearableData.RData")
+
+######################################################################################################
+## TASK 5: CREATE A SECOND INDEPENDENT DATASET WITH THE AVERAGE OF EACH VARIABLE FOR EACH ACTIVITY  ##
+## AND SUBJECT                                                                                      ##
+######################################################################################################
+
+tidyDataAvg <- aggregate(tidyData, list(subjectid=tidyData$subjectid, description=tidyData$description),
+                              FUN=mean,na.rm=TRUE, na.action=NUL)
+tidyDataAvg <- tidyDataAvg[,c(1:2,5:ncol(tidyDataAvg))]  ## removes extra columns
+
+
+## add meaningful column name, reflect fact that columns are averages
+avgColumnNames <- names(tidyDataAvg)
+prefixLabel <- "Avg"
+prefixedColumnNames <- paste(prefixLabel,avgColumnNames[3:length(avgColumnNames)], sep="")
+avgColumnNames <- c("subjectid", "description",  prefixedColumnNames)
+names(tidyDataAvg) <- avgColumnNames
+
+## Data set to a file
+WearableDataAvg <-  tidyDataAvg
+save(WearableDataAvg, file="WearableDataAvg.RData")
